a0fbca7f3394ad9c1933c48363dd3ee796e841a6
Shreyansh1005 <145058833+Shreyansh1005@users.noreply.github.com>
2024-10-06 12:48:07 +0530

    removing logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import KevinRushlogo from "../assets/KevinRushLogo.png";
+// import KevinRushlogo from "../assets/KevinRushLogo.png";
 
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -6,9 +6,10 @@ import { FaGithub } from "react-icons/fa";
 const Navbar = ()=>{
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
-            <div className="flex flex-shrink-0 items-center">
+{/*             <
+div className="flex flex-shrink-0 items-center">
                 <img className = "mx-2 w-10" src={KevinRushlogo} alt="logo"/>
-            </div>
+            </div> */}
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <FaLinkedinIn/>
                 <FaGithub />
@@ -20,4 +21,5 @@ const Navbar = ()=>{
 };
 
 export default Navbar;
+
 
