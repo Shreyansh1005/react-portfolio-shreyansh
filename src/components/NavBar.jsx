4523343025b51a137470c1b71e1f2b98e25542b7
Shreyansh1005 <145058833+Shreyansh1005@users.noreply.github.com>
2024-10-06 12:46:11 +0530

    Update NavBar.jsx twice

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-// import logo from "../assets/KevinRushLogo.png";
+import KevinRushlogo from "../assets/KevinRushLogo.png";
 
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -7,7 +7,7 @@ const Navbar = ()=>{
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className = "mx-2 w-10" src={logo} alt="logo"/>
+                <img className = "mx-2 w-10" src={KevinRushlogo} alt="logo"/>
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <FaLinkedinIn/>
@@ -20,3 +20,4 @@ const Navbar = ()=>{
 };
 
 export default Navbar;
+
