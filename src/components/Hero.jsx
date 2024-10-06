5d6306160b62cfda2d81dec7d889425bf076d2c9
Shivam Dubey <shivam.2000shivu@gmail.com>
2024-10-06 13:59:20 +0530

    Updating the size of the image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () =>{
                         className="my-2 max-w-xl py-6 font-light tracking-tighter"> {HERO_CONTENT}</motion.p>
                     </div>
                 </div>
-                <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="w-full lg:w-3/4 lg:p-8">
                         <div className="flex justify-center">
                             <motion.img initial={{x: 100, opacity: 0}}
                             animate = {{x:0,opacity:1}}
