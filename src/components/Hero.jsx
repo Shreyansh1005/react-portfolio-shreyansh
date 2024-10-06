882db677e1ad7efcc77a727d95322f1890a5bd6d
Shivam Dubey <shivam.2000shivu@gmail.com>
2024-10-06 13:55:30 +0530

    Added my image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from  "../constants";
-import profilePic from "../assets/kevinRushProfile.jpg";
+import profilePic from "../assets/my_image.jpg";
 import {delay, motion} from "framer-motion";
 
 const container = (delay) =>({
